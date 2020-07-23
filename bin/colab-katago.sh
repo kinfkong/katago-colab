e2e9b8f52f8572fd282ece577e0b12345042a891
kinfkong <kinfkong@126.com>
2020-07-23 18:25:56 +0800

    update command

--- bin/colab-katago.sh
+++ bin/colab-katago.sh
@@ -6,5 +6,10 @@ then
     exit -1
 fi
 curl --silent https://kata-config.oss-cn-beijing.aliyuncs.com/$USER_NAME.ssh.txt -o /tmp/ssh.txt
+curl --silent https://kata-config.oss-cn-beijing.aliyuncs.com/$USER_NAME.pem -o /tmp/ssh.pem
+
+chmod 600 /tmp/ssh.pem
+
 SSH_CMD=`cat /tmp/ssh.txt`
-ssh $SSH_CMD
+
+ssh -i /tmp/ssh.pem $SSH_CMD
