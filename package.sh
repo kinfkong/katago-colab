78fb298b72f37fcf1c7df82db5f3eeb4177de4d6
kinfkong <kinfkong@126.com>
2020-07-24 17:09:36 +0800

    fixes

--- package.sh
+++ package.sh
@@ -4,12 +4,12 @@ GOOS=linux GOARCH=amd64 go build -o ./bin/colab-katago-for-linux
 GOOS=windows GOARCH=amd64 go build -o ./bin/colab-katago-for-windows
 
 cd bin
-rm -rf *.tar
+rm -rf *.zip
 cp colab-katago-for-mac colab-katago
-tar cvf colab-katago.mac.tar colab-katago
+zip colab-katago.mac.zip colab-katago
 cp colab-katago-for-linux colab-katago
-tar cvf colab-katago.linux.tar colab-katago
+zip colab-katago.linux.zip colab-katago
 cp colab-katago-for-windows colab-katago.exe
-tar cvf colab-katago.windows.tar colab-katago.exe
+zip colab-katago.windows.zip colab-katago.exe
 rm colab-katago colab-katago.exe
 cd -
